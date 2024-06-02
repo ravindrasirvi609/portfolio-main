a3fb0bf7e03a6503d83a3e600ecc47ba96936aeb
Ravindra <sirviravindra609@gmail.com>
2024-06-02 20:43:10 +0530

    feat: Add lazy loading for images in VisitorData component

---
+++ app/visitor-data/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const VisitorData = () => {
+  const [visitorList, setVisitorList] = useState<
+    {
+      latitude: number;
+      longitude: number;
+      location?: string;
+      createdAt: string;
+      _id: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post("/api/visitorList");
+
+        const visitorsWithLocation = await Promise.all(
+          response.data.visitorList.map(async (visitor: any) => {
+            try {
+              const locationResponse = await axios.get(
+                `https://api.opencagedata.com/geocode/v1/json?q=${visitor.latitude}+${visitor.longitude}&key=${process.env.NEXT_PUBLIC_OPENCAGE_API_KEY}`
+              );
+
+              return {
+                ...visitor,
+                location:
+                  locationResponse.data.results[0]?.formatted ||
+                  "Unknown location",
+              };
+            } catch (locationError) {
+              console.error("Error fetching location data:", locationError);
+              return { ...visitor, location: "Error fetching location" };
+            }
+          })
+        );
+
+        setVisitorList(visitorsWithLocation);
+      } catch (error) {
+        console.error("Error fetching visitor list:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Visitor List</h1>
+      <table className="min-w-full table-auto bg-red-500 rounded-lg overflow-hidden border border-spacing-4">
+        <thead>
+          <tr>
+            <th className="p-2">Latitude</th>
+            <th className="p-2">Longitude</th>
+            <th className="p-2">Location</th>
+            <th className="p-2">Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {visitorList.map((visitor) => (
+            <tr key={visitor._id}>
+              <td className="p-2">{visitor.latitude}</td>
+              <td className="p-2">{visitor.longitude}</td>
+              <td className="p-2">
+                {visitor.location ? visitor.location : "Loading location..."}
+              </td>
+              <td className="p-2">
+                {new Date(visitor.createdAt).toLocaleString()}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default VisitorData;

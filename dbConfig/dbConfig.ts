c0bc46f8f8e0d14b29d2976bcfccfe4d02c33e68
Ravindra <sirviravindra609@gmail.com>
2024-06-02 17:01:29 +0530

    chore: Remove unnecessary console.log statement in dbConfig.ts

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -4,7 +4,6 @@ export async function connect() {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
-    console.log("+++++++++++++++++++++++" + process.env.MONGO_URI!);
 
     connection.on("connected", () => {
       console.log("MongoDB connected successfully");
